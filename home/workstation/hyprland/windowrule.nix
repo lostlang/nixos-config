8c3c66326e80b555781a6402a7550340b25c00e6
Alec Lang <lostlang@icloud.com>
2025-04-29 23:22:28 +0500

    fix: windowrule

--- home/workstation/hyprland/windowrule.nix
+++ home/workstation/hyprland/windowrule.nix
@@ -1,14 +1,24 @@
 {
-
   wayland.windowManager.hyprland.settings = {
     # hyprctl clients
     windowrule = [
-      "float,class:nm-connection-editor"
-      "pin,class:nm-connection-editor"
-      "float, title:Picture-in-Picture"
-      "pin, title:Picture-in-Picture"
-      "size 640 360, title:Picture-in-Picture"
-      "move 1280 0, title:Picture-in-Picture"
+      "float, nm-connection-editor"
+      "pin, nm-connection-editor"
+
+      "float, blueman-manager-wrapped"
+      "pin, blueman-manager-wrapped"
+
+      "float, blueman-applet-wrapped"
+      "pin, blueman-applet-wrapped"
+      "move 1600 0, blueman-applet-wrapped"
+
+      "float, title:^(Picture-in-Picture)"
+      "pin, title:^(Picture-in-Picture)"
+      "size 640 360, title:^(Picture-in-Picture)"
+      "move 1280 0, title:^(Picture-in-Picture)"
+
+      "workspace 3, librewolf"
+      "workspace 10, org.telegram.desktop"
     ];
   };
 }
