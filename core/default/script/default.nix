d6eefe710b2b69d8f68c5eb18a2d50811cab1f5f
Alec Lang <lostlang@icloud.com>
2025-07-19 11:06:46 +0500

    feat: add minecrat script

--- core/default/script/default.nix
+++ core/default/script/default.nix
@@ -2,6 +2,7 @@
 {
   environment.systemPackages = [
     (import ./clean_script.nix { inherit pkgs; })
+    (import ./minecraft_data_copy.nix { inherit pkgs; })
     (import ./ollama_model_tester.nix { inherit pkgs; })
     (import ./steam_clip_builder.nix { inherit pkgs; })
   ];
