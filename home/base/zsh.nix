d918066a03163052bab1880f55a5d1a575f86798
Alec Lang <lostlang@icloud.com>
2025-10-02 22:15:46 +0500

    deps: update

--- home/base/zsh.nix
+++ home/base/zsh.nix
@@ -12,7 +12,7 @@
       {
         rebuild-home = "home-manager switch --flake ${flakeDir}";
         rebuild-os = "sudo nixos-rebuild switch --flake ${flakeDir}";
-        update-flake = "nix flake update ${flakeDir}";
+        update-flake = ''nix flake update --flake "path:${flakeDir}"'';
         btop = "btop -p 1";
         v = "zellij --layout=code";
         nix-shell = "nix-shell --run $SHELL";
