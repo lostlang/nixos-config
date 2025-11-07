4a19bff8f923fa46a2b4d6408eec6a9a4051fa77
Alec Lang <lostlang@icloud.com>
2025-11-07 20:59:21 +0500

    feat: update go flake

--- env/go.flake.nix
+++ env/go.flake.nix
@@ -1,20 +1,27 @@
 {
   description = "Go dev shell (flake)";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # https://www.nixhub.io/packages/go
+    go-nixpkgs.url = "";
+  };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, go-nixpkgs, ... }:
     let
       system = builtins.currentSystem;
       pkgs = import nixpkgs { inherit system; };
+      goPkgs = import go-nixpkgs { inherit system; };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         name = "go-dev";
-        buildInputs = with pkgs; [ go ];
+        buildInputs = with goPkgs; [ go ];
 
         shellHook = ''
+          export GOTOOLCHAIN=local
           echo "🐹 Go env: $(go version)"
         '';
       };
