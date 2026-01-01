47831d9ef71cc1132f818167a21084e1b79d1d0c
Alec Lang <lostlang@icloud.com>
2026-01-02 00:30:55 +0500

    chore: change aider colors

--- system/base/script/aiderw.nix
+++ system/base/script/aiderw.nix
@@ -2,12 +2,21 @@
 pkgs.writeShellScriptBin "aiderw" ''
   exec ${pkgs.aider-chat}/bin/aider \
     --watch-files \
+    --user-input-color "#437F06" \
+    --tool-output-color "#10072B" \
+    --tool-error-color "#AE2626" \
+    --tool-warning-color "#9B8318" \
+    --assistant-output-color "#23569F" \
+    --completion-menu-bg-color "#EDDFBF" \
+    --completion-menu-color "#10072B" \
+    --completion-menu-current-bg-color "#437F06" \
+    --completion-menu-current-color "#10072B" \
+    --code-theme "autumn" \
     --no-gitignore \
     --notifications \
     --no-show-model-warnings \
     --no-auto-commits \
     --no-dirty-commits \
-    --chat-language "ru" \
-    --edit-format "diff" \
+    --edit-format diff \
     "$@"
 ''
